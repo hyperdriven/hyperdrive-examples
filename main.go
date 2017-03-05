508c13e8f40e51ede1d4694d7c21d67b8bcb1f52
Eric <eric.marden@gmail.com>
2017-03-04 20:37:00 -0600

    updates examples for v0.0.2

--- main.go
+++ main.go
@@ -18,8 +18,20 @@ func (e *ExampleEndpoint) Get(rw http.ResponseWriter, r *http.Request) {
 
 var Example = &ExampleEndpoint{hyperdrive.Endpoint{"Example Endpoint", "Example Endpoint, written with hyperdriven/hyperdrive", "/example"}}
 
+type PanicEndpoint struct {
+	hyperdrive.Endpoint
+}
+
+func (e *PanicEndpoint) Get(rw http.ResponseWriter, r *http.Request) {
+	panic("ERROR! Everyone panic!")
+	return
+}
+
+var PanicExample = &PanicEndpoint{hyperdrive.Endpoint{"Panic Endpoint", "This example tests panic recovery.", "/panic"}}
+
 func main() {
 	api := hyperdrive.NewAPI()
 	api.AddEndpoint(Example)
-	http.ListenAndServe(":8080", api.Router)
+	api.AddEndpoint(PanicExample)
+	api.Start()
 }
